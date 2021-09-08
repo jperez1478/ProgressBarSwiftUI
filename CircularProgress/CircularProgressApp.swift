5e64b3c6498a36fbc613470428420fa9f817eb1a
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-09-08 18:58:05 -0500

    Initial Commit

---
+++ CircularProgress/CircularProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularProgressApp.swift
+//  CircularProgress
+//
+//  Created by Jessica Perez on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct CircularProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
